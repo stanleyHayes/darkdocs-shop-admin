afc3496fa9ef51b076d3bf2de046f0c32057d719
Stanley Hayford <stanley_hayford@vienhealth.com>
2021-08-01 10:12:27 +0000

    Change server url to production

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,4 @@
-export const SERVER_BASE_URL = 'http://localhost:5000/api/v1';
-// export const PRODUCTION_HEROKU_SERVER = 'https://darkdocs-shop.herokuapp.com/api/v1';
+// export const SERVER_BASE_URL = 'http://localhost:5000/api/v1';
+export const SERVER_BASE_URL = 'https://darkdocs-shop.herokuapp.com/api/v1';
 export const DARKDOCS_SHOP_ADMIN_USER_KEY = 'DARKDOCS_SHOP_ADMIN_USER_KEY';
 export const DARKDOCS_SHOP_ADMIN_TOKEN_KEY = 'DARKDOCS_SHOP_ADMIN_TOKEN_KEY';
-export const DARKDOCS_SHOP_ADMIN_INSTRUCTIONS_KEY = 'DARKDOCS_SHOP_ADMIN_INSTRUCTIONS_KEY';
